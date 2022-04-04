8217d362325ef0e23065bcdb03b232731861b766
bngsh <hee132@korea.ac.kr>
2022-04-04 15:17:50 +0000

    add nginx and uwsgi

--- CHEMBTI/settings.py
+++ CHEMBTI/settings.py
@@ -133,6 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+##### 2022/03/27 BNGSH for web server #####
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+#####################################
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
